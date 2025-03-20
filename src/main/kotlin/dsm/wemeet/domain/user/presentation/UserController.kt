a966b262c611050da4f2f518e42f6d2a48cf61f5
soohyeon <meltapplee@gmail.com>
2025-03-20 23:19:55 +0900

    feat :: controller repository

---
+++ src/main/kotlin/dsm/wemeet/domain/user/presentation/UserController.kt
@@ -0,0 +1,26 @@
+package dsm.wemeet.domain.user.presentation
+
+import dsm.wemeet.domain.user.presentation.dto.request.SignInRequest
+import dsm.wemeet.domain.user.presentation.dto.request.SignUpRequest
+import dsm.wemeet.domain.user.usecase.UserSignInUseCase
+import dsm.wemeet.domain.user.usecase.UserSignUpUseCase
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/user")
+class UserController(
+    private val userSignUpUseCase: UserSignUpUseCase,
+    private val userSignInUseCase: UserSignInUseCase
+) {
+
+    @PostMapping("/signUp")
+    fun signUp(@RequestBody request: SignUpRequest) =
+        userSignUpUseCase.execute(request)
+
+    @PostMapping("/signIn")
+    fun signIn(@RequestBody request: SignInRequest) =
+        userSignInUseCase.execute(request)
+}
