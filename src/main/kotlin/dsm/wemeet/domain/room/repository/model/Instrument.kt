11e87d51f947e692de3cb821ce941bdebcaf25c3
khyunwook <khyunwook0707@gmail.com>
2025-03-20 09:53:31 +0900

    feat :: User

---
+++ src/main/kotlin/dsm/wemeet/domain/room/repository/model/Instrument.kt
@@ -0,0 +1,9 @@
+package dsm.wemeet.domain.room.repository.model
+
+enum class Instrument {
+    VOCAL,
+    PIANO,
+    DRUM,
+    GUITAR,
+    ETC
+}
---
+++ src/main/kotlin/dsm/wemeet/domain/user/repository/model/User.kt
@@ -0,0 +1,28 @@
+package dsm.wemeet.domain.user.repository.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+
+@Entity
+class User(
+
+    @Id
+    @Column(columnDefinition = "VARCHAR(50)", nullable = false)
+    val email: String,
+
+    @Column(columnDefinition = "CHAR(255)", nullable = false)
+    val password: String,
+
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false, name = "account_id")
+    val accountId: String,
+
+    @Column(columnDefinition = "CHAR(255)", nullable = true)
+    val profile: String,
+
+    @Column(columnDefinition = "VARCHAR(255)", nullable = true, name = "about_me")
+    val aboutMe: String,
+
+    @Column(columnDefinition = "VARCHAR(40)", nullable = true)
+    val position: String
+)
