7ae1cee39e002f1072af19119b2ce6786a1b4ac4
soohyeon <meltapplee@gmail.com>
2025-03-20 17:17:09 +0900

    feat: jwt 구성 추가

--- src/main/kotlin/dsm/wemeet/WeMeetApplication.kt
+++ src/main/kotlin/dsm/wemeet/WeMeetApplication.kt
@@ -1,8 +1,10 @@
 package dsm.wemeet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class WeMeetApplication
 
---
+++ src/main/kotlin/dsm/wemeet/global/jwt/JwtProperties.kt
@@ -0,0 +1,16 @@
+package dsm.wemeet.global.jwt
+
+import io.jsonwebtoken.security.Keys
+import org.springframework.boot.context.properties.ConfigurationProperties
+import javax.crypto.SecretKey
+
+@ConfigurationProperties("jwt")
+class JwtProperties(
+    secret: String,
+    val accessExp: Long,
+    val refreshExp: Long,
+    val header: String,
+    val prefix: String
+) {
+    val secret: SecretKey = Keys.hmacShaKeyFor(secret.toByteArray())
+}
