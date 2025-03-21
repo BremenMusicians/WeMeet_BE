dcebf25ef6deb6b76531b111250e274330c5812a
khyunwook <khyunwook0707@gmail.com>
2025-03-21 16:21:07 +0900

    feat :: Service

--- src/main/kotlin/dsm/wemeet/domain/friend/repository/FriendJpaRepository.kt
+++ src/main/kotlin/dsm/wemeet/domain/friend/repository/FriendJpaRepository.kt
@@ -2,6 +2,19 @@ package dsm.wemeet.domain.friend.repository
 
 import dsm.wemeet.domain.friend.repository.model.Friend
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.UUID
 
-interface FriendJpaRepository : JpaRepository<Friend, UUID>
+interface FriendJpaRepository : JpaRepository<Friend, UUID> {
+
+    @Query(
+        """
+        SELECT 1
+        FROM Friend as f
+        WHERE f.proposer.email = :user1 AND f.receiver.email = :user2 OR
+            f.receiver.email = :user1 AND f.proposer.email = :user2
+        """
+    )
+    fun findByAnyEmail(@Param("user1") user1: String, @Param("user2") user2: String): Friend?
+}
--- src/main/kotlin/dsm/wemeet/domain/friend/service/impl/CheckFriendServiceImpl.kt
+++ src/main/kotlin/dsm/wemeet/domain/friend/service/impl/CheckFriendServiceImpl.kt
@@ -1,10 +1,18 @@
 package dsm.wemeet.domain.friend.service.impl
 
+import dsm.wemeet.domain.friend.exception.FriendAlreadyExistsException
 import dsm.wemeet.domain.friend.repository.FriendJpaRepository
 import dsm.wemeet.domain.friend.service.CheckFriendService
+import dsm.wemeet.domain.user.repository.model.User
 import org.springframework.stereotype.Service
 
 @Service
 class CheckFriendServiceImpl(
     private val friendJpaRepository: FriendJpaRepository
-) : CheckFriendService
+) : CheckFriendService {
+
+    override fun checkIsAlreadyFriend(user1: User, user2: User) {
+        friendJpaRepository.findByAnyEmail(user1.email, user2.email)
+            ?: throw FriendAlreadyExistsException
+    }
+}
