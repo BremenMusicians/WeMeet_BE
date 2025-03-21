aaebcbb013d951e199df2112108830bfe2868fd4
khyunwook <khyunwook0707@gmail.com>
2025-03-21 19:45:27 +0900

    refact :: lint

--- src/main/kotlin/dsm/wemeet/domain/friend/usecase/RequestFriendUseCase.kt
+++ src/main/kotlin/dsm/wemeet/domain/friend/usecase/RequestFriendUseCase.kt
@@ -6,7 +6,6 @@ import dsm.wemeet.domain.friend.service.CommandFriendService
 import dsm.wemeet.domain.user.service.QueryUserService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.UUID
 
 @Service
 @Transactional
