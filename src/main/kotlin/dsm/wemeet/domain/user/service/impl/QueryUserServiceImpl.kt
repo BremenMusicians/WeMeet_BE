4e0b92a85ccede1804bb41b1d4666e194d4fd624
soohyeon <meltapplee@gmail.com>
2025-03-21 09:48:36 +0900

    chore :: 서비스 레이어로 에러 분리

--- src/main/kotlin/dsm/wemeet/domain/user/service/impl/QueryUserServiceImpl.kt
+++ src/main/kotlin/dsm/wemeet/domain/user/service/impl/QueryUserServiceImpl.kt
@@ -1,5 +1,7 @@
 package dsm.wemeet.domain.user.service.impl
 
+import dsm.wemeet.domain.user.exception.UserAlreadyExistException
+import dsm.wemeet.domain.user.exception.UserNotFoundException
 import dsm.wemeet.domain.user.repository.UserJpaRepository
 import dsm.wemeet.domain.user.service.QueryUserService
 import org.springframework.stereotype.Service
@@ -10,14 +12,21 @@ class QueryUserServiceImpl(
 ) : QueryUserService {
 
     override fun queryUserByEmail(email: String) =
-        userJpaRepository.findByEmail(email)
+        userJpaRepository.findByEmail(email) ?: throw UserNotFoundException
 
     override fun queryUserByAccountId(accountId: String) =
-        userJpaRepository.findByAccountId(accountId)
+        userJpaRepository.findByAccountId(accountId) ?: throw UserNotFoundException
 
-    override fun existsByEmail(email: String) =
-        userJpaRepository.existsByEmail(email)
+    override fun existsByEmail(email: String): Boolean {
+        return checkExists(userJpaRepository.existsByEmail(email))
+    }
 
-    override fun existsByAccountId(accountId: String) =
-        userJpaRepository.existsByAccountId(accountId)
+    override fun existsByAccountId(accountId: String): Boolean {
+        return checkExists(userJpaRepository.existsByAccountId(accountId))
+    }
+
+    private fun checkExists(exists: Boolean): Boolean {
+        if (exists) throw UserAlreadyExistException
+        return exists
+    }
 }
