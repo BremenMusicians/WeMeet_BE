0403e96b88b4623deee711639a01dba248d6e66c
khyunwook <khyunwook0707@gmail.com>
2025-03-21 14:08:07 +0900

    feat :: UseCase, Controller

--- src/main/kotlin/dsm/wemeet/domain/friend/presentation/FriendController.kt
+++ src/main/kotlin/dsm/wemeet/domain/friend/presentation/FriendController.kt
@@ -1,19 +1,22 @@
 package dsm.wemeet.domain.friend.presentation
 
-import dsm.wemeet.domain.friend.usecase.ApplyFriendUseCase
+import dsm.wemeet.domain.friend.usecase.RequestFriendUseCase
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
 
 @RestController
 @RequestMapping("/friends")
 class FriendController(
-    private val applyFriendUseCase: ApplyFriendUseCase
+    private val requestFriendUseCase: RequestFriendUseCase
 ) {
 
-    @PostMapping("/{user_id}")
-    fun applyFriend(@PathVariable("user_id") userId: UUID): UUID =
-        applyFriendUseCase.execute(userId)
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/{account-id}")
+    fun requestFriend(@PathVariable("account-id") accountId: String) {
+        requestFriendUseCase.execute(accountId)
+    }
 }
---
+++ src/main/kotlin/dsm/wemeet/domain/friend/usecase/RequestFriendUseCase.kt
@@ -0,0 +1,34 @@
+package dsm.wemeet.domain.friend.usecase
+
+import dsm.wemeet.domain.friend.repository.model.Friend
+import dsm.wemeet.domain.friend.service.CheckFriendService
+import dsm.wemeet.domain.friend.service.CommandFriendService
+import dsm.wemeet.domain.user.service.QueryUserService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Service
+@Transactional
+class RequestFriendUseCase(
+    private val queryUserService: QueryUserService,
+    private val checkFriendService: CheckFriendService,
+    private val commandFriendService: CommandFriendService
+) {
+
+    fun execute(accountId: String) {
+        val proposal = queryUserService.getCurrentUser()
+        val receiver = queryUserService.queryUserByAccountId(accountId)
+
+        checkFriendService.checkIsAlreadyFriend(proposal, receiver)
+
+        commandFriendService.saveFriend(
+            Friend(
+                id = UUID.randomUUID(),
+                proposer = proposal,
+                receiver = receiver,
+                isAccepted = false
+            )
+        )
+    }
+}
