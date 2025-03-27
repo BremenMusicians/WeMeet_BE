27770fefb7a954a4e2a5b524545bb2ffe0792c70
khyunwook <khyunwook0707@gmail.com>
2025-03-27 13:39:45 +0900

    feat :: service 구현 명시

--- src/main/kotlin/dsm/wemeet/domain/room/service/impl/CheckRoomServiceImpl.kt
+++ src/main/kotlin/dsm/wemeet/domain/room/service/impl/CheckRoomServiceImpl.kt
@@ -1,6 +1,7 @@
 package dsm.wemeet.domain.room.service.impl
 
+import dsm.wemeet.domain.room.service.CheckRoomService
 import org.springframework.stereotype.Service
 
 @Service
-class CheckRoomServiceImpl
+class CheckRoomServiceImpl : CheckRoomService
--- src/main/kotlin/dsm/wemeet/domain/room/service/impl/QueryRoomServiceImpl.kt
+++ src/main/kotlin/dsm/wemeet/domain/room/service/impl/QueryRoomServiceImpl.kt
@@ -1,6 +1,7 @@
 package dsm.wemeet.domain.room.service.impl
 
+import dsm.wemeet.domain.room.service.QueryRoomService
 import org.springframework.stereotype.Service
 
 @Service
-class QueryRoomServiceImpl
+class QueryRoomServiceImpl : QueryRoomService
