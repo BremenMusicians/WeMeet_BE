a932fa5bfec21d8052f92e47b8f2266c7d30ce8c
soohyeon <meltapplee@gmail.com>
2025-03-28 13:55:37 +0900

    chore :: 필요없는 예외 삭제

--- src/main/kotlin/dsm/wemeet/global/jwt/auth/UserDetailService.kt
+++ src/main/kotlin/dsm/wemeet/global/jwt/auth/UserDetailService.kt
@@ -1,7 +1,6 @@
 package dsm.wemeet.global.jwt.auth
 
 import dsm.wemeet.domain.user.service.QueryUserService
-import dsm.wemeet.global.jwt.exception.InvalidTokenException
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
@@ -13,7 +12,6 @@ class UserDetailService(
 
     override fun loadUserByUsername(accountId: String): UserDetails {
         val user = queryUserService.queryUserByAccountId(accountId)
-            ?: throw InvalidTokenException
 
         return UserDetails(user.accountId)
     }
