522a6d80f8b36236b2abbc36e855d89027cce102
soohyeon <meltapplee@gmail.com>
2025-04-03 22:55:47 +0900

    chore :: aws 의존성, yml 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-mail:3.3.0")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("com.amazonaws:aws-java-sdk-s3:1.12.130")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
     compileOnly("org.projectlombok:lombok")
