c85387ddae2b35ad07c525e44f33a9952467e989
soohyeon <meltapplee@gmail.com>
2025-04-15 14:33:56 +0900

    chore :: 로직 변경

--- src/main/kotlin/dsm/wemeet/domain/message/usecase/QueryMessageUseCase.kt
+++ src/main/kotlin/dsm/wemeet/domain/message/usecase/QueryMessageUseCase.kt
@@ -1,5 +1,6 @@
 package dsm.wemeet.domain.message.usecase
 
+import dsm.wemeet.domain.chat.service.QueryChatService
 import dsm.wemeet.domain.message.presentation.dto.response.MessageResponse
 import dsm.wemeet.domain.message.service.QueryMessageService
 import org.springframework.stereotype.Service
@@ -9,18 +10,20 @@ import java.util.UUID
 @Transactional(readOnly = true)
 @Service
 class QueryMessageUseCase(
-    private val queryMessageService: QueryMessageService
+    private val queryMessageService: QueryMessageService,
+    private val queryChatService: QueryChatService
 ) {
 
-    fun execute(chatId: UUID): List<MessageResponse?> {
-        val messages = queryMessageService.queryMessageListByChat(chatId)
-
-        return messages?.map {
-            MessageResponse(
-                sender = it.sender.email,
-                content = it.content,
-                sendAt = it.sendAt
-            )
-        } ?: emptyList()
+    fun execute(chatId: UUID): List<MessageResponse> {
+        val chat = queryChatService.queryChatById(chatId)
+        return queryMessageService.queryMessageListByChat(chat.id!!)
+            .orEmpty()
+            .map {
+                MessageResponse(
+                    sender = it.sender.email,
+                    content = it.content,
+                    sendAt = it.sendAt
+                )
+            }.sortedBy { it.sendAt }
     }
 }
