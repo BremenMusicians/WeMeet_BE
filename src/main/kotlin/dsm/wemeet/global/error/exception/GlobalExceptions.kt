b20a6821b43d992dc12f50b7d59a367db9ba8f35
khyunwook <khyunwook0707@gmail.com>
2025-04-18 12:06:49 +0900

    refactor :: 누락 코드;;

--- src/main/kotlin/dsm/wemeet/global/error/exception/GlobalExceptions.kt
+++ src/main/kotlin/dsm/wemeet/global/error/exception/GlobalExceptions.kt
@@ -2,6 +2,10 @@ package dsm.wemeet.global.error.exception
 
 import WeMeetException
 
+object UnauthorizedException : WeMeetException(
+    ErrorCode.UNAUTHORIZED
+)
+
 object BadRequestException : WeMeetException(
     ErrorCode.BAD_REQUEST
 )
