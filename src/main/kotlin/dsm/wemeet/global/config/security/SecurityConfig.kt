f538766433a867a20eaba48c2bc4776043c0656a
soohyeon <meltapplee@gmail.com>
2025-04-22 13:46:37 +0900

    fix :: 웹소켓 403

--- src/main/kotlin/dsm/wemeet/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/dsm/wemeet/global/config/security/SecurityConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -64,6 +65,13 @@ class SecurityConfig(
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web ->
+            web.ignoring().requestMatchers("/chat", "/rooms/**")
+        }
+    }
 
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
