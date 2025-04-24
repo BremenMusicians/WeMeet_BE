4303c401a5b3640ab0343120f568141bcd7191fe
khyunwook <khyunwook0707@gmail.com>
2025-04-24 13:45:41 +0900

    feat :: repository

--- src/main/kotlin/dsm/wemeet/domain/friend/repository/FriendJpaRepository.kt
+++ src/main/kotlin/dsm/wemeet/domain/friend/repository/FriendJpaRepository.kt
@@ -1,6 +1,7 @@
 package dsm.wemeet.domain.friend.repository
 
 import dsm.wemeet.domain.friend.repository.model.Friend
+import dsm.wemeet.domain.user.repository.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -27,4 +28,21 @@ interface FriendJpaRepository : JpaRepository<Friend, UUID> {
     """
     )
     fun findAcceptedFriendsByUser(@Param("user") user: String): List<Friend>
+
+    @Query(
+        """
+            SELECT CASE WHEN f.proposer.email = :email
+                THEN f.receiver
+                ELSE f.proposer
+            END
+            FROM Friend f
+            WHERE (f.receiver.email = :email OR f.proposer.email = :email)
+                AND f.isAccepted = TRUE
+                AND CASE WHEN f.proposer.email = :email
+                THEN f.receiver.accountId
+                ELSE f.proposer.accountId
+            END LIKE CONCAT('%', :accountId, '%')
+        """
+    )
+    fun findFriendUsersByEmailContainsAccountId(@Param("email") email: String, @Param("accountId") accountId: String): List<User>
 }
