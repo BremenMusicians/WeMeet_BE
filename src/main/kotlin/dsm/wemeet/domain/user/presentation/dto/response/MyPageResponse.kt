3501d3510eb51d9cb7f0cd6135eeb2b727168d7e
soohyeon <meltapplee@gmail.com>
2025-04-25 16:58:31 +0900

    chore :: myPage 리스폰스 변경

--- src/main/kotlin/dsm/wemeet/domain/user/presentation/dto/response/MyPageResponse.kt
+++ src/main/kotlin/dsm/wemeet/domain/user/presentation/dto/response/MyPageResponse.kt
@@ -1,13 +1,10 @@
 package dsm.wemeet.domain.user.presentation.dto.response
 
-import dsm.wemeet.domain.friend.presentation.dto.response.FriendResponse
 import dsm.wemeet.domain.user.repository.model.Position
 
 data class MyPageResponse(
     val accountId: String,
     val profile: String?,
     val aboutMe: String?,
-    val position: List<Position>,
-    val friendsCnt: Int,
-    val friends: List<FriendResponse>
+    val position: List<Position>
 )
