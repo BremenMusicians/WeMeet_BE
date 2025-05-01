ee27aec8e317610e89f9186879c8f31b9c0ef492
khyunwook <khyunwook0707@gmail.com>
2025-05-01 10:30:46 +0900

    feat :: UseCase

---
+++ src/main/kotlin/dsm/wemeet/domain/room/usecase/UpdateMemberPositionUseCase.kt
@@ -0,0 +1,25 @@
+package dsm.wemeet.domain.room.usecase
+
+import dsm.wemeet.domain.room.service.QueryRoomService
+import dsm.wemeet.domain.user.repository.model.Position
+import dsm.wemeet.domain.user.service.QueryUserService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Service
+@Transactional
+class UpdateMemberPositionUseCase(
+    private val queryRoomService: QueryRoomService,
+    private val queryUserService: QueryUserService
+) {
+
+    fun execute(roomId: UUID, userEmail: String, position: Position) {
+        val currentRoom = queryRoomService.queryRoomById(roomId)
+        val currentUser = queryUserService.queryUserByEmail(userEmail)
+
+        val currentMember = queryRoomService.queryMemberByUserEmailAndRoomId(userEmail, roomId)
+
+        currentMember.position = position
+    }
+}
