fe8982a87c2b7b42aa83e93ca8641dd973164dd7
soohyeon <meltapplee@gmail.com>
2025-05-01 17:27:39 +0900

    burn :: chatController 삭제

--- src/main/kotlin/dsm/wemeet/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/dsm/wemeet/global/config/security/SecurityConfig.kt
@@ -55,7 +55,6 @@ class SecurityConfig(
                 .requestMatchers(HttpMethod.DELETE, "/rooms/{room-id}").authenticated()
                 .requestMatchers(HttpMethod.POST, "/rooms/password/{room-id}").authenticated()
                 .requestMatchers(HttpMethod.DELETE, "/rooms/{room-id}/members/{account-id}").authenticated()
-                .requestMatchers(HttpMethod.GET, "/chat/list").authenticated()
                 .requestMatchers(HttpMethod.GET, "/message/{chat-id}").authenticated()
                 .anyRequest().denyAll()
         }
