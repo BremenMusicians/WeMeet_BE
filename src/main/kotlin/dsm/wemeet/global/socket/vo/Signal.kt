b79e6397eea2eeef1c34b3241a3c499d1c2f5ef4
khyunwook <khyunwook0707@gmail.com>
2025-05-09 10:16:30 +0900

    chore :: Signal에 from 추가

--- src/main/kotlin/dsm/wemeet/global/socket/vo/Signal.kt
+++ src/main/kotlin/dsm/wemeet/global/socket/vo/Signal.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode
 
 data class Signal(
     val type: String,
+    val from: String? = null,
     val to: String? = null,
     val data: JsonNode? = null
 )
